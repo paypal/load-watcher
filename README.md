88bc8037498ff3f2d1c8485d5c60a0288e40bbf4
Abdul Qadeer <aqadeer@paypal.com>
2020-11-06 09:16:00 -0800

    Add watcher and metricsprovider packages Add fetcher clients for SignalFx and K8s Add UTs
    Signed-off-by: Abdul Qadeer <aqadeer@paypal.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Load Watcher
+
+The load watcher is responsible for the cluster-wide aggregation of resource usage metrics like CPU, memory, network, and IO stats over time windows from a metrics provider like SignalFx, Prometheus, K8s etc.
+It stores them in its local cache, which can be queried from scheduler plugins.
---
+++ pkg/metricsprovider/k8s.go
@@ -0,0 +1,123 @@
+/*
+Copyright 2020 PayPal
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metricsprovider
+
+import (
+	"context"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"strconv"
+
+	"metrics/loadwatcher/pkg/watcher"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
+)
+
+var (
+	isLocal = false
+)
+
+const (
+	localKubeConfig = "/Users/aqadeer/.kube/config" // path to local kube config file
+	localDev        = "LOCAL_DEV"                   // env variable to indicate client is outside the cluster usu. for local testing
+)
+
+func init() {
+	v := os.Getenv(localDev)
+	val, err := strconv.ParseBool(v)
+	if err == nil {
+		isLocal = val
+	}
+}
+
+// This is a client for K8s provided Metric Server
+type metricsServerClient struct {
+	metricsClientSet *metricsv.Clientset   // This client fetches node metrics from metric server
+	coreClientSet    *kubernetes.Clientset // This client fetches node capacity
+}
+
+func NewMetricsServerClient() (watcher.FetcherClient, error) {
+	var config *rest.Config
+	var err error
+	kubeConfig := ""
+	if isLocal {
+		kubeConfig = localKubeConfig
+	}
+	config, err = clientcmd.BuildConfigFromFlags("", kubeConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	metricsClientSet, err := metricsv.NewForConfig(config)
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return metricsServerClient{
+		metricsClientSet: metricsClientSet,
+		coreClientSet:    clientSet}, nil
+}
+
+func (m metricsServerClient) FetchHostMetrics(host string, window *watcher.Window) ([]watcher.Metric, error) {
+	var metrics = []watcher.Metric{}
+
+	nodeMetrics, err := m.metricsClientSet.MetricsV1beta1().NodeMetricses().Get(context.TODO(), host, metav1.GetOptions{})
+	if err != nil {
+		return metrics, err
+	}
+	var fetchedMetric watcher.Metric
+	node, err := m.coreClientSet.CoreV1().Nodes().Get(context.Background(), host, metav1.GetOptions{})
+	if err != nil {
+		return metrics, err
+	}
+	fetchedMetric.Value = float64(100*nodeMetrics.Usage.Cpu().MilliValue()) / float64(node.Status.Capacity.Cpu().MilliValue())
+	fetchedMetric.Type = watcher.CPU
+	metrics = append(metrics, fetchedMetric)
+	return metrics, nil
+}
+
+func (m metricsServerClient) FetchAllHostsMetrics(window *watcher.Window) (map[string][]watcher.Metric, error) {
+	metrics := make(map[string][]watcher.Metric)
+
+	nodeMetricsList, err := m.metricsClientSet.MetricsV1beta1().NodeMetricses().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return metrics, err
+	}
+	nodeList, err := m.coreClientSet.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return metrics, err
+	}
+	nodeCapacityMap := make(map[string]int64)
+	for _, host := range nodeList.Items {
+		nodeCapacityMap[host.Name] = host.Status.Capacity.Cpu().MilliValue()
+	}
+	for _, host := range nodeMetricsList.Items {
+		var fetchedMetric watcher.Metric
+		fetchedMetric.Type = watcher.CPU
+		if _, ok := nodeCapacityMap[host.Name]; !ok {
+			log.Errorf("unable to find host %v in node list", host.Name)
+			continue
+		}
+		fetchedMetric.Value = float64(host.Usage.Cpu().MilliValue()) / float64(nodeCapacityMap[host.Name])
+		metrics[host.Name] = append(metrics[host.Name], fetchedMetric)
+	}
+	return metrics, nil
+}
